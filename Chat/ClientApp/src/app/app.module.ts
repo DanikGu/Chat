bde0029e36652ac15d5896f8164f1b733630557b
DanikGu <97472739+DanikGu@users.noreply.github.com>
2022-02-03 11:40:46 +0200

    Add icon for left menu

--- Chat/ClientApp/src/app/app.module.ts
+++ Chat/ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -33,7 +33,8 @@ import { HomeComponent } from './Components/home/home.component';
   providers: [
     [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
 }
